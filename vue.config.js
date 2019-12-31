9e6ad837402037f53f26a11fd8309ca886a5ecca
machengrong@css.com.cn <machengrong@css.com.cn>
2019-12-31 17:11:26 +0800

    update:移除代理

--- vue.config.js
+++ vue.config.js
@@ -62,7 +62,7 @@ module.exports = {
     proxy: {
       '/': {
         ws: false,
-        target: 'https://cssg.gddj.gov.cn/', // dev环境 测试
+        target: '', // dev环境 测试
         changeOrigin: true,
         pathRewrite: {
           '^/': '/' // 这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
