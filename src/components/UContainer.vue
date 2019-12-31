1ce9bc77aba9ff9399e016ffa5cc78a4b7a58ec5
machengrong@css.com.cn <machengrong@css.com.cn>
2019-12-31 17:06:19 +0800

    update:尾部表单

--- src/components/UContainer.vue
+++ src/components/UContainer.vue
@@ -16,10 +16,10 @@
         </el-row>
       </div>
       <el-row type="flex" class="page-layout header-menu">
-        <el-col class="header-menu_each">
+        <el-col class="header-menu_each" :class="current.includes(routeMap.index) || '/' ? 'active' : ''">
           <h3 @click="toPage(routeMap.index)">首页</h3>
         </el-col>
-        <el-col class="header-menu_each">
+        <el-col class="header-menu_each" :class="current.includes(routeMap.class) ? 'active' : ''">
           <h3>
             <el-dropdown @command="handleCommand">
               <span class="el-dropdown-link">
@@ -33,16 +33,16 @@
             </el-dropdown>
           </h3>
         </el-col>
-        <el-col class="header-menu_each">
+        <el-col class="header-menu_each" :class="current.includes(routeMap.guide) ? 'active' : ''">
           <h3 @click="toPage(routeMap.guide)">特色晚辅</h3>
         </el-col>
-        <el-col class="header-menu_each">
+        <el-col class="header-menu_each" :class="current.includes(routeMap.teacher) ? 'active' : ''">
           <h3 @click="toPage(routeMap.teacher)">名师介绍</h3>
         </el-col>
-        <el-col class="header-menu_each">
+        <el-col class="header-menu_each" :class="current.includes(routeMap.information) ? 'active' : ''">
           <h3 @click="toPage(routeMap.information)">资讯中心</h3>
         </el-col>
-        <el-col class="header-menu_each">
+        <el-col class="header-menu_each" :class="current.includes(routeMap.about) ? 'active' : ''">
           <h3 @click="toPage(routeMap.about)">关于我们</h3>
         </el-col>
       </el-row>
@@ -51,6 +51,39 @@
       <slot></slot>
     </el-main>
     <el-footer class="footer" height="auto">
+      <div class="footer-form">
+        <el-row type="flex" class="page-layout">
+          <el-col :span="6">
+
+          </el-col>
+          <el-col :span="18">
+            <el-form :inline="true" :model="consult">
+              <el-form-item>
+                <el-input v-model="consult.user" placeholder="您的名字"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-input v-model="consult.phone" placeholder="手机"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-select v-model="consult.grade">
+                  <el-option label="小学" value="little"></el-option>
+                  <el-option label="初中" value="junior"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item>
+                <el-select v-model="consult.class">
+                  <el-option label="英语" value="english"></el-option>
+                  <el-option label="数学" value="math"></el-option>
+                  <el-option label="语文" value="chinese"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="onSubmit">免费咨询</el-button>
+              </el-form-item>
+            </el-form>
+          </el-col>
+        </el-row>
+      </div>
       <el-row type="flex" class="page-layout footer-list">
         <el-col class="footer-each">
           <h3>关于我们</h3>
@@ -128,13 +161,18 @@
   export default {
     data() {
       return {
-        current: '',
-        routeMap: routeMap
+        current: '' || location.pathname,
+        routeMap: routeMap,
+        consult: {
+          user: '',
+          phone: '',
+          grade: 'little',
+          class: 'english'
+        }
       }
     },
     components: {},
     mounted() {
-
     },
     methods: {
       toPage(page) {
@@ -142,6 +180,9 @@
       },
       handleCommand(command) {
         windowOpenUrl(routeMap.guide, { type: command })
+      },
+      onSubmit() {
+
       }
     }
   }
@@ -178,6 +219,15 @@
     background-color: #fff;
     color: @fontColor;
   }
+  /deep/ .el-button--primary{
+    background-color: #de9021;
+    border-color: #de9021;
+  }
+  /deep/ .el-button--primary.is-active,/deep/ .el-button--primary:active,
+  /deep/ .el-button--primary:focus,/deep/  .el-button--primary:hover{
+    background-color: #de9060;
+    border-color: #de9060;
+  }
 
   .header {
     .header-meta {
@@ -193,7 +243,8 @@
 
         h3 {
           color: @fontColor;
-          font-size: 18px;
+          font-size: 20px;
+          font-weight: normal;
         }
       }
 
@@ -206,18 +257,24 @@
   .footer {
     background-color: @footerBg;
     color: @footerColor;
-
-    h3 {
-      margin-bottom: 20px;
+    .footer-form{
+      padding-top: 60px;
+      background-color: @themeColor;
     }
+    .footer-list{
+      padding-top: 45px;
+      h3 {
+        margin-bottom: 20px;
+      }
 
-    .footer-each {
-      text-align: center;
-      border-right: 1px solid @footerColor;
+      .footer-each {
+        text-align: center;
+        border-right: 1px solid @footerColor;
 
-      .footer-menu {
-        line-height: 32px;
-        cursor: pointer;
+        .footer-menu {
+          line-height: 32px;
+          cursor: pointer;
+        }
       }
     }
   }
