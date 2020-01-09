cc75ea1d2365eac4fb568dbdc7df08286ca67590
machengrong@css.com.cn <machengrong@css.com.cn>
2020-01-09 15:40:32 +0800

    update:teacher

--- src/pages/class/App.vue
+++ src/pages/class/App.vue
@@ -42,15 +42,45 @@
           </el-col>
           <el-col :span="6">
             <div class="class-card">
-              <h3 class="class-card_title">名师一对一</h3>
+              <h3 class="class-card_title" style="margin-bottom: 25px">名师一对一</h3>
               <ul class="class-card_des">
                 <li>量身定制的教学设计</li>
                 <li>专属的学员提升方案</li>
               </ul>
-              <a class="class-card_btn">立即咨询</a>
+              <a class="class-card_btn" style="margin-top: 37px;">立即咨询</a>
             </div>
           </el-col>
         </el-row>
+      </div>
+      <div class="teacher-wrap">
+        <h2 class="special-title"><span>名师出高徒</span>整体感知，局部理解，挖掘中心思想</h2>
+        <div class="teacher-list page-layout clearfix">
+          <div class="teacher-list_tab">
+            <div class="teacher-tab" :class="teacherIdx == index ? 'active' : ''" v-for="(item,index) in teacherList" :key="index" @click="teacherIdx = index">
+              <img :src="item.tlogo">
+              {{item.tname}}老师
+            </div>
+          </div>
+          <div class="teacher-list_desc">
+            <div class="teacher-desc" :class="teacherIdx == index ? 'active' : ''" v-for="(item,index) in  teacherList" :key="index">
+              <img class="teacher-desc_logo" :src="item.tBlogo">
+              <div class="teacher-desc_main">
+                <h3 class="teacher-desc_title"><span>{{item.tname}}</span>{{item.description.class}}</h3>
+                <ul class="teacher-desc_detail">
+                  <li>
+                    <h4>个人见解</h4>
+                    <p>{{item.description.desc}}</p>
+                  </li>
+                  <li>
+                    <h4>教学风格：</h4>
+                    <p>{{item.description.type}}</p>
+                  </li>
+                </ul>
+                <a class="teacher-desc_btn">立即咨询</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="map-wrap">
         <UMap :styleObj="{width: '1200px',margin: '0 auto',padding:'20px',backgroundColor:'#fff'}"></UMap>
@@ -69,7 +99,38 @@
     name: 'Math',
     data() {
       return {
-        lesson: ''
+        lesson: '',
+        teacherIdx: 0,
+        teacherList: [
+          {
+            tlogo: require('../../assets/images/logo.png'),
+            tname: '白冰',
+            tBlogo: require('../../assets/images/student.png'),
+            description: {
+              class: '五到六年级数学',
+              desc: '华南理工大学硕士，五年小升初经验、三年中考经验，熟悉小学和初中数学教学体系，\n' +
+                '                      能够准确把握学科重难点。培训过的多名学员参报数学竞赛并获奖，多名优秀学员考入华英、南实、\n' +
+                '                      南外和黄岐石门的重点班并获取奖学金。',
+              type: '幽默诙谐且爱探究，力求让学生“知其然且知其所以然”，注重思维引导和培养学生的自主\n' +
+                '                      思考与流畅表达的能力。',
+              qq: ''
+            }
+          },
+          {
+            tlogo: require('../../assets/images/logo.png'),
+            tname: '白冰1',
+            tBlogo: require('../../assets/images/student.png'),
+            description: {
+              class: '五到六年级数学',
+              desc: '华南理工大学硕士，五年小升初经验、三年中考经验，熟悉小学和初中数学教学体系，\n' +
+                '                      能够准确把握学科重难点。培训过的多名学员参报数学竞赛并获奖，多名优秀学员考入华英、南实、\n' +
+                '                      南外和黄岐石门的重点班并获取奖学金。',
+              type: '幽默诙谐且爱探究，力求让学生“知其然且知其所以然”，注重思维引导和培养学生的自主\n' +
+                '                      思考与流畅表达的能力。',
+              qq: ''
+            }
+          }
+        ]
       }
     },
     components: {
@@ -89,19 +150,23 @@
 </script>
 <style scoped lang='less'>
   @import "../../styles/common.less";
-  .special-wrap{
-    background-color: #f9f2f2;
+  .commonMix(@bg:#f9f2f2){
+    background-color: @bg;
     padding: 20px 0;
     text-align: center;
-    h2.special-title{
-      font-size: 36px;
-      font-weight: bolder;
-      margin: 40px 0 28px;
-      span{
-        padding:0 30px 0  0;
-        color: #f08519;
-      }
+  }
+  h2.special-title{
+    font-size: 36px;
+    font-weight: bolder;
+    margin: 40px 0 28px;
+    span{
+      padding:0 30px 0  0;
+      color: #f08519;
     }
+  }
+
+  .special-wrap{
+    .commonMix();
     p.special-desc{
       color:#332c2b;
       font-size: 26px;
@@ -139,26 +204,126 @@
     }
   }
   .class-wrap{
-    background-color: #f9f2f2;
-    padding: 20px 0;
-    text-align: center;
-    h2.special-title{
-      font-size: 36px;
-      font-weight: bolder;
-      margin: 40px 0 28px;
-      span{
-        padding:0 30px 0  0;
-        color: #f08519;
-      }
-    }
+    .commonMix();
     .class-card{
       display: inline-block;
       margin: 0 auto;
       border:1px solid #d0d0d0;
-      padding: 20px 10px 10px;
+      padding: 20px 25px 10px;
       &_title{
         color:#10623f;
         font-size: 22px;
+        line-height: 36px;
+        margin-bottom: 10px;
+      }
+      &_des li{
+        color:#332c2b;
+        line-height: 32px;
+        font-weight: bold;
+        text-align: center;
+      }
+      &_btn{
+        background-color: #83c034;
+        color:#fff;
+        border-radius: 12px;
+        font-weight: bold;
+        text-align: center;
+        display: inline-block;
+        width: 175px;
+        line-height: 36px;
+        margin-top: 20px;
+        cursor: pointer;
+      }
+    }
+  }
+  .teacher-wrap{
+    .commonMix(#d5e2b8);
+    .teacher-list{
+      .teacher-list_tab{
+        float: right;
+        background-color: #d7e8b6;
+        .teacher-tab{
+          &.active{
+            background-color: #acd176;
+          }
+          padding:10px 20px;
+          font-weight: bold;
+          color: #332c2b;
+          cursor: pointer;
+          &:not(:last-child){
+            border-bottom: 1px solid #fff;
+          }
+          img{
+            width: 45px;
+            object-fit: cover;
+            vertical-align: middle;
+            border-radius: 50%;
+            border: 2px solid #83c034;
+          }
+        }
+      }
+      .teacher-list_desc{
+        float: right;
+        .teacher-desc.active{
+          display: block;
+        }
+        .teacher-desc{
+          position: relative;
+          display: none;
+          .teacher-desc_logo{
+            position: absolute;
+            left: -210px;
+            bottom:-20px;
+          }
+          .teacher-desc_main{
+            display: inline-block;
+            width: 680px;
+            padding: 30px 20px 10px 10px;
+            text-align: left;
+            h3{
+              font-weight: normal;
+              font-size:18px;
+              line-height: 36px;
+              border-bottom: 2px solid #83c034;
+              span{
+                font-weight: bold;
+                font-size: 32px;
+                padding-bottom: 7px;
+                border-bottom:2px solid #e57721;
+              }
+            }
+            .teacher-desc_detail{
+              padding: 15px 0 10px;
+              li{
+                background-color: #acd176;
+                padding: 10px;
+                line-height: 24px;
+                h4{
+
+                }
+                p{
+                  font-size: 14px;
+                  text-indent: 4em;
+                }
+
+              }
+              li+li{
+                margin-top: 5px;
+              }
+            }
+            .teacher-desc_btn{
+              display: inline-block;
+              line-height: 26px;
+              text-align: center;
+              border-radius: 12px;
+              background-color: #e57721;
+              color:#fff;
+              font-size: 14px;
+              padding: 0 20px;
+              cursor: pointer;
+            }
+          }
+        }
       }
     }
   }
